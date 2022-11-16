aea520be85810cafe285648fe71c83ec3bc70720
Majestic Taco <90423367+MajesticTaco@users.noreply.github.com>
2022-11-16 08:46:08 +0200

    Update atslegas.py

---
+++ atslegas.py
@@ -0,0 +1,14 @@
+import tkinter
+import customtkinter
+import csv
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry("400x580")
+app.title("Atslēgu datubāze")
+
+
+
+app.mainloop()
